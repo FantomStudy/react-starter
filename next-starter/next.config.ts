75fb0dbcd9f966a70f48792ede53cfadadcf1964
FantomStudy <arzamastsevdaniil@gmail.com>
2026-05-19 22:13:40 +0500

    chore: remove legacy configs

--- next-starter/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typedRoutes: true,
-  reactStrictMode: true,
-  output: "standalone",
-};
-
-export default nextConfig;
